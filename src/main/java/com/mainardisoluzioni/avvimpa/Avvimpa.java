92b1ab63c7b78bae6f3751db8f878fd078cadd57
Davide Mainardi <davide@mainardisoluzioni.com>
2024-03-09 19:33:52 +0100

    Si riesce a creare il PDF.

--- src/main/java/com/mainardisoluzioni/avvimpa/Avvimpa.java
+++ src/main/java/com/mainardisoluzioni/avvimpa/Avvimpa.java
@@ -1,5 +1,18 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Copyright (C) 2024 Davide Mainardi <davide@mainardisoluzioni.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package com.mainardisoluzioni.avvimpa;
 
@@ -26,11 +39,13 @@ public class Avvimpa {
         System.out.println("Hello World!");
         
         List<Object> tempBean = new ArrayList<>();
+        tempBean.add(new Object());
         Map<String, Object> params = new HashMap<>();
-        InputStream inputStream = Avvimpa.class.getResourceAsStream("/com/mainardisoluzioni/avvimpa/documents/etichetta.jasper");
+        Avvimpa instance = new Avvimpa();
+        InputStream inputStream = instance.getFileAsIOStream("documents/etichetta.jasper");
         try {
             JasperPrint jasperPrint = JasperFillManager.fillReport(inputStream, params, new JRBeanCollectionDataSource(tempBean));
-            JasperExportManager.exportReportToPdfFile(jasperPrint, "maina");
+            JasperExportManager.exportReportToPdfFile(jasperPrint, "maina.pdf");
         } catch (JRException ex) {
             Logger.getLogger(Avvimpa.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -47,5 +62,15 @@ public class Avvimpa {
         e.printStackTrace();
         }
         comPort.closePort();*/
+    }
+    
+    private InputStream getFileAsIOStream(final String fileName) 
+    {
+        InputStream ioStream = this.getClass().getClassLoader().getResourceAsStream(fileName);
+        
+        if (ioStream == null) {
+            throw new IllegalArgumentException(fileName + " is not found");
+        }
+        return ioStream;
     }
 }
