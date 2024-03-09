a8749a6fc5ca9b33a85d5a42c9a2674bcac71a07
Davide Mainardi <davide@mainardisoluzioni.com>
2024-03-09 18:06:40 +0100

    Capito come generare il JAR con le dipendenze grazie a https://stackoverflow.com/a/574650

--- src/main/java/com/mainardisoluzioni/avvimpa/Avvimpa.java
+++ src/main/java/com/mainardisoluzioni/avvimpa/Avvimpa.java
@@ -3,9 +3,18 @@
  */
 package com.mainardisoluzioni.avvimpa;
 
-import com.fazecast.jSerialComm.SerialPort;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 
 /**
  *
@@ -15,19 +24,28 @@ public class Avvimpa {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-
-        SerialPort comPort = SerialPort.getCommPorts()[0];
+        
+        List<Object> tempBean = new ArrayList<>();
+        Map<String, Object> params = new HashMap<>();
+        InputStream inputStream = Avvimpa.class.getResourceAsStream("/com/mainardisoluzioni/avvimpa/documents/etichetta.jasper");
+        try {
+            JasperPrint jasperPrint = JasperFillManager.fillReport(inputStream, params, new JRBeanCollectionDataSource(tempBean));
+            JasperExportManager.exportReportToPdfFile(jasperPrint, "maina");
+        } catch (JRException ex) {
+            Logger.getLogger(Avvimpa.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        /*SerialPort comPort = SerialPort.getCommPorts()[0];
         comPort.openPort();
         comPort.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING, 0, 0);
         InputStream in = comPort.getInputStream();
         try {
-            for (int j = 0; j < 1000; ++j) {
-                System.out.print((char) in.read());
-            }
-            in.close();
-        } catch (Exception e) {
-            e.printStackTrace();
+        for (int j = 0; j < 1000; ++j) {
+        System.out.print((char) in.read());
         }
-        comPort.closePort();
+        in.close();
+        } catch (Exception e) {
+        e.printStackTrace();
+        }
+        comPort.closePort();*/
     }
 }
