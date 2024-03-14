0ed8f809be2e1f6f3dcdca568916ff461d6f2c3d
Davide Mainardi <davide@mainardisoluzioni.com>
2024-03-14 13:46:24 +0100

    Installato la etichettatrice e iniziato a stampare. Installare il driver TOSHIBA B-FV4-G.

--- src/main/java/com/mainardisoluzioni/avvimpa/Avvimpa.java
+++ src/main/java/com/mainardisoluzioni/avvimpa/Avvimpa.java
@@ -23,11 +23,23 @@ import java.util.List;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.print.PrintService;
+import javax.print.attribute.HashPrintRequestAttributeSet;
+import javax.print.attribute.HashPrintServiceAttributeSet;
+import javax.print.attribute.PrintRequestAttributeSet;
+import javax.print.attribute.PrintServiceAttributeSet;
+import javax.print.attribute.standard.MediaPrintableArea;
+import javax.print.attribute.standard.MediaSize;
+import javax.print.attribute.standard.OrientationRequested;
+import javax.print.attribute.standard.PrinterName;
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.engine.export.JRPrintServiceExporter;
+import net.sf.jasperreports.export.SimpleExporterInput;
+import net.sf.jasperreports.export.SimplePrintServiceExporterConfiguration;
 
 /**
  *
@@ -36,9 +48,11 @@ import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 public class Avvimpa {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        
-        List<Object> tempBean = new ArrayList<>();
+
+        Avvimpa instance = new Avvimpa();
+        instance.stampaSuEtichettatrice("mainami");
+
+        /*List<Object> tempBean = new ArrayList<>();
         tempBean.add(new Object());
         Map<String, Object> params = new HashMap<>();
         Avvimpa instance = new Avvimpa();
@@ -48,7 +62,9 @@ public class Avvimpa {
             JasperExportManager.exportReportToPdfFile(jasperPrint, "maina.pdf");
         } catch (JRException ex) {
             Logger.getLogger(Avvimpa.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        }*/
+
+
         /*SerialPort comPort = SerialPort.getCommPorts()[0];
         comPort.openPort();
         comPort.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING, 0, 0);
@@ -63,14 +79,68 @@ public class Avvimpa {
         }
         comPort.closePort();*/
     }
-    
-    private InputStream getFileAsIOStream(final String fileName) 
+
+    private InputStream getFileAsIOStream(final String fileName)
     {
         InputStream ioStream = this.getClass().getClassLoader().getResourceAsStream(fileName);
-        
+
         if (ioStream == null) {
             throw new IllegalArgumentException(fileName + " is not found");
         }
         return ioStream;
     }
+
+    private void stampaSuEtichettatrice(String nomeEtichettatrice) {
+        if (nomeEtichettatrice != null && !nomeEtichettatrice.isBlank()) {
+            PrintService printService = PrintUtility.findPrintService(nomeEtichettatrice);
+            if (printService != null) {
+                try {
+                    JasperPrint jasperPrint = creaReport();
+
+                    PrintRequestAttributeSet printRequestAttributeSet = new HashPrintRequestAttributeSet();
+                    printRequestAttributeSet.add(new MediaPrintableArea(0, 0, 1, 0.5f, MediaPrintableArea.INCH));
+                    printRequestAttributeSet.add(MediaSize.findMedia(1, 0.5f, MediaSize.INCH));
+                    printRequestAttributeSet.add(OrientationRequested.PORTRAIT);
+
+                    PrintServiceAttributeSet printServiceAttributeSet = new HashPrintServiceAttributeSet();
+                    printServiceAttributeSet.add(printService.getAttribute(PrinterName.class));
+
+                    JRPrintServiceExporter exporter = new JRPrintServiceExporter();
+                    exporter.setExporterInput(new SimpleExporterInput(jasperPrint));
+                    SimplePrintServiceExporterConfiguration configuration = new SimplePrintServiceExporterConfiguration();
+
+                    //configuration.setPrintRequestAttributeSet(printRequestAttributeSet);
+                    configuration.setPrintServiceAttributeSet(printServiceAttributeSet);
+                    configuration.setDisplayPageDialog(false);
+                    configuration.setDisplayPrintDialog(false);
+                    exporter.setConfiguration(configuration);
+                    exporter.exportReport();
+                    
+                    JasperExportManager.exportReportToPdfFile(jasperPrint, "maina.pdf");
+                } catch (JRException ex) {
+                    Logger.getLogger(Avvimpa.class.getName()).log(Level.SEVERE, null, ex);
+                    //Messages.create("error").error().detail(ex.getMessage()).add();
+                }
+            }
+            else
+                Logger.getLogger(Avvimpa.class.getName()).log(Level.SEVERE, null, "vendita.etichetta.stampa.errore.etichettatriceMancante");
+                //Messages.create("error").error().detail("vendita.etichetta.stampa.errore.etichettatriceMancante").add();
+        }
+    }
+
+    private JasperPrint creaReport() throws JRException {
+        List<Object> tempBean = new ArrayList<>();
+        Map<String, Object> params = new HashMap<>();
+        params.put("ReportTitle", "Etichetta");
+        /*params.put("subReportPath", externalContext.getRealPath("/resources/documents/vendita/") + "/");
+        params.put("reportImagePath", externalContext.getRealPath("/resources/documents/images/") + "/");*/
+
+        tempBean.add(new Object());
+
+        Avvimpa instance = new Avvimpa();
+        InputStream inputStream = instance.getFileAsIOStream("documents/etichetta.jasper");
+        return JasperFillManager.fillReport(inputStream, params, new JRBeanCollectionDataSource(tempBean));
+    }
+
+
 }
