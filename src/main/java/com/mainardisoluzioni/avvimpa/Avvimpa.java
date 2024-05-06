e7d7b3e32ad6cea79f1b310d70c53af80fa5c3c5
Amministratore Avvvimpa Test <adminavvimpa@avvimpa-001>
2024-05-06 09:25:35 +0200

    Aggiornato il codice in modo che sia necessario utilizzare il lettore del codice a barre (con o senza l'avvitatore).

--- src/main/java/com/mainardisoluzioni/avvimpa/Avvimpa.java
+++ src/main/java/com/mainardisoluzioni/avvimpa/Avvimpa.java
@@ -30,8 +30,11 @@ import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.print.PrintService;
+import javax.print.attribute.HashPrintRequestAttributeSet;
 import javax.print.attribute.HashPrintServiceAttributeSet;
+import javax.print.attribute.PrintRequestAttributeSet;
 import javax.print.attribute.PrintServiceAttributeSet;
+import javax.print.attribute.standard.Copies;
 import javax.print.attribute.standard.PrinterName;
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperFillManager;
@@ -54,53 +57,124 @@ public class Avvimpa {
     }
     
     public void ascoltaSullaSerialeEStampaEtichetta() {
-        boolean isBarcode = false;
-        SerialPort comPort = null;
+        boolean isBarcode = false;       
+        SerialPort comPortBar = null;
+        SerialPort comPortAvv = null;
         for (SerialPort commPortTemp : SerialPort.getCommPorts()) {
             String portName = commPortTemp.getDescriptivePortName().toLowerCase();
-            if (portName.contains("usb") || portName.contains("barcode")) {
-                comPort = commPortTemp;
-                if (portName.contains("barcode"))
-                    isBarcode = true;
-                break;
+            if (portName.contains("barcode")) {
+                comPortBar = commPortTemp;
+                isBarcode = true;
+            }
+            if(portName.contains("usb")){
+                comPortAvv = commPortTemp;
             }
         }
-        if (comPort != null) {
-            comPort.disableExclusiveLock();
-            comPort.openPort();
-            comPort.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING, 0, 0);
-            try (InputStream in = comPort.getInputStream()) {
-                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    if (isBarcode)
-                        stampaSuEtichettatrice(nomeEtichettatrice, line + " OK", DateTimeFormatter.ofPattern("dd/MM/yy HH:mm:ss").format(LocalDateTime.now()));
-                    else {
+                
+
+        if (comPortAvv == null) {
+            comPortBar.disableExclusiveLock();
+            comPortBar.openPort();
+            comPortBar.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING, 0, 0);
+            int qtaEtichette = 1;
+            try (InputStream inBar = comPortBar.getInputStream()) {
+                BufferedReader readerBar = new BufferedReader(new InputStreamReader(inBar));
+                String lineBar;
+                while ((lineBar = readerBar.readLine()) != null) {
+                    
+                        
                         try {
-                            String subLine = line.substring(76);
-                            if (subLine.toLowerCase().contains("program end")) {
-                                //System.out.println(subLine.substring(0, 17));
-                                stampaSuEtichettatrice(nomeEtichettatrice, "Avvitatura OK", subLine.substring(0, 17));
-                            }
-                        } catch (IndexOutOfBoundsException e) {
-                            // niente da fare
+                        String subLine = lineBar.substring(0);  
+                        qtaEtichette = Integer.parseInt(subLine.substring(0, 1));                       
+                        stampaSuEtichettatrice(nomeEtichettatrice, subLine.substring(1, 6) + " OK", DateTimeFormatter.ofPattern("dd/MM/yy HH:mm:ss").format(LocalDateTime.now()), qtaEtichette);
+                        }catch (IndexOutOfBoundsException e) {
                         }
-                    }
+                    
                 }
             } catch (IOException ex) {
                 Logger.getLogger(Avvimpa.class.getName()).log(Level.SEVERE, null, ex);
             }
-            comPort.closePort();
+            
+            comPortBar.closePort();
+            
         }
+        
+    if (comPortAvv != null) {
+        
+         System.out.println("presente avvitatore");
+         
+            comPortBar.disableExclusiveLock();
+            comPortBar.openPort();
+            comPortBar.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING, 0, 0);
+            int qtaEtichette = 1;
+            try (InputStream inBar = comPortBar.getInputStream()) {
+                BufferedReader readerBar = new BufferedReader(new InputStreamReader(inBar));
+                String lineBar;
+                while ((lineBar = readerBar.readLine()) != null) {
+                    
+                        
+                        try {
+                        String subLine = lineBar.substring(0);  
+                        
+                        //System.out.println(subLine.substring(0, 1));
+                        //qtaEtichette = Integer.parseInt(subLine.substring(0, 1));
+                        
+                        //System.out.println(qtaEtichette + "/"+ subLine.substring(1, 6));
+                        
+                        //stampaSuEtichettatrice(nomeEtichettatrice, subLine.substring(1, 6) + " OK", DateTimeFormatter.ofPattern("dd/MM/yy HH:mm:ss").format(LocalDateTime.now()), qtaEtichette);
+                        
+
+                        comPortAvv.disableExclusiveLock();
+                        comPortAvv.openPort();
+                        comPortAvv.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING, 0, 0);
+
+                        try (InputStream inAvv = comPortAvv.getInputStream()) {
+                            BufferedReader readerAvv = new BufferedReader(new InputStreamReader(inAvv));
+                            String lineAvv;
+                            while ((lineAvv = readerAvv.readLine()) != null) {
+                               
+                            try {
+                                String subLineAvv = lineAvv.substring(76);
+                                if (subLineAvv.toLowerCase().contains("program end")) {
+                                  stampaSuEtichettatrice(nomeEtichettatrice, subLine.substring(1, 6), subLineAvv.substring(0, 17), qtaEtichette);
+                                 }
+                             } catch (IndexOutOfBoundsException e) {
+                              // niente da fare
+                            }                        
+                        
+                            }
+                        
+                            }catch (IndexOutOfBoundsException e) {
+                        }
+                            
+                        }catch (IndexOutOfBoundsException e) {
+                        }
+                    
+                }
+            } catch (IOException ex) {
+                Logger.getLogger(Avvimpa.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+            comPortBar.closePort();
+            comPortAvv.closePort();
+                        
+        }    
+        
     }
 
-    private void stampaSuEtichettatrice(String nomeEtichettatrice, String operazione, String identificativo) {
+
+  
+
+    private void stampaSuEtichettatrice(String nomeEtichettatrice, String operazione, String identificativo, int qtaEtichette) {
         if (nomeEtichettatrice != null && !nomeEtichettatrice.isBlank()) {
             PrintService printService = PrintUtility.findPrintService(nomeEtichettatrice);
             if (printService != null) {
                 try {
                     JasperPrint jasperPrint = creaReport(operazione, identificativo);
 
+                    PrintRequestAttributeSet printRequestAttributeSet = new HashPrintRequestAttributeSet();
+                    printRequestAttributeSet.add(new Copies(qtaEtichette));
+                    
                     PrintServiceAttributeSet printServiceAttributeSet = new HashPrintServiceAttributeSet();
                     printServiceAttributeSet.add(printService.getAttribute(PrinterName.class));
 
@@ -109,6 +183,7 @@ public class Avvimpa {
                     SimplePrintServiceExporterConfiguration configuration = new SimplePrintServiceExporterConfiguration();
 
                     configuration.setPrintServiceAttributeSet(printServiceAttributeSet);
+                    configuration.setPrintRequestAttributeSet(printRequestAttributeSet);
                     configuration.setDisplayPageDialog(false);
                     configuration.setDisplayPrintDialog(false);
                     exporter.setConfiguration(configuration);
